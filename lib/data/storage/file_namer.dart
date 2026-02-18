716732a74935c86baadd47e6c9d8e2d8ec14628a
MaksishinSergey <ser.maksishin@gmail.com>
2026-02-18 09:51:14 +0300

    Changed name format

--- lib/data/storage/file_namer.dart
+++ lib/data/storage/file_namer.dart
@@ -1,11 +1,11 @@
 import 'package:intl/intl.dart';
 
 class FileNamer {
-  static String outputClipName({required String fio, required String apparatus}) {
+  static String outputClipName({required String id, required String fio, required String city}) {
     final ts = DateFormat('yyyyMMdd_HHmmss').format(DateTime.now());
     final safeFio = _sanitize(fio);
-    final safeApp = _sanitize(apparatus);
-    return '${safeFio}_${safeApp}_$ts.mp4';
+    final safeApp = _sanitize(city);
+    return '$id ${safeFio}_$safeApp-$ts.mp4';
   }
 
   static String _sanitize(String value) {
